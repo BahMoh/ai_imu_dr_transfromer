d1735122b97bcec1f393b2ef8afa5ee2c542cf5e
unknown <mohamadai2573@gmail.com>
2024-06-25 14:02:48 +0330

    fj

--- src/seq_to_seq_transformer.py
+++ src/seq_to_seq_transformer.py
@@ -153,6 +153,10 @@ class PositionalEncoding(nn.Module):
         pe = torch.zeros(1, max_len, d_model)
         pe[0, :, 0::2] = torch.sin(position * div_term)
         pe[0, :, 1::2] = torch.cos(position * div_term)
+        print(f"position {position.shape}")
+        print(f"exp_term {exp_term.shape}")
+        print(f"div_term {div_term.shape}")
+        print(f"pe {pe.shape}")
         self.register_buffer("pe", pe)
 
     def forward(self, x):
