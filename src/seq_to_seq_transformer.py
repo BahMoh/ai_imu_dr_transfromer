cd1136a84e5bbb5b2342777e468deb9f438d7210
unknown <mohamadai2573@gmail.com>
2024-06-26 11:36:50 +0330

    Hi there

--- src/seq_to_seq_transformer.py
+++ src/seq_to_seq_transformer.py
@@ -65,8 +65,8 @@ class MultiHeadAttention(nn.Module):
         # Compute attention score
         # (N, h, T, d_k) x (N, h, d_k, T) -> (N, h, T, T)
         attn_scores = q @ k.transpose(-2, -1) / math.sqrt(self.d_k)
+        attn_scores.to(device)
         if pad_mask is not None:
-            print(type(pad_mask))
             attn_scores = attn_scores.masked_fill(
                 pad_mask[:, None, None, :] == 0, float("-inf"))
 
